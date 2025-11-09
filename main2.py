9446dc76619bd26e8bb91bc25edb8a7b03a0399d
Jasonjeff250 <etimitah@gmail.com>
2025-11-09 14:57:32 +0100

    Add files via upload

--- main2.py
+++ main2.py
@@ -1,3 +1,6 @@
 #ask the user to enter a name
 name=input("Enter your name:")
 print(f"Hello, {name} welcome to my program!")
+age=int(input("Enter your age:"))
+print(f"You are {age} years old.")
+print(f"Your name is {name} and you are {age} years old.")
