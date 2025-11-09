3e8011d3e18d4cd2694c89472f288199e597342f
Jason <etimitah@gmail.com>
2025-11-09 14:41:37 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#check for datatypes
+name="Jason"
+age=13
+grade=10
+weight=60
+is_student=True
+#display the datatypes
+print("The datatype of name is:", type(name))
+print("The datatype of age is:", type(age))
+print("The datatype of grade is:", type(grade))
+print("The datatype of weight is:", type(weight))
+print("The datatype of is_student is:", type(is_student))
+str("20+25")
---
+++ main2.py
@@ -0,0 +1,3 @@
+#ask the user to enter a name
+name=input("Enter your name:")
+print(f"Hello, {name} welcome to my program!")
